af61830f4744eb4e1b3eb995b117deb730d15c2d
who <38827824+gggg907877781@users.noreply.github.com>
2019-04-10 10:28:20 +0800

    Update HelloWorld.java

--- testGit/src/test1/HelloWorld.java
+++ testGit/src/test1/HelloWorld.java
@@ -3,7 +3,7 @@ package test1;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
-		System.out.println("ssssssssssssssss");
+		System.out.println("bbbbbbbbbbbbbb");
 		System.out.println("wwwwwwwwwwwwwwwwww");
 		System.out.println("aaaaaa");
 	}
