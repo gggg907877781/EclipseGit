72fcb0a2123ad96ecdecd26f7b52727ef2481067
4gdxv <907877781@qq.com>
2019-04-10 10:38:27 +0800

    hello1

--- testGit/src/test1/HelloWorld.java
+++ testGit/src/test1/HelloWorld.java
@@ -2,15 +2,10 @@ package test1;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello World !");
-		System.out.println("ssssssssssssssss");
-		System.out.println("wwwwwwwwwwwwwwwwww");
-=======
 		System.out.println("Hello World !");
 		System.out.println("bbbbbbbbbbbbbb");
 		System.out.println("wwwwwwwwwwwwwwwwww");
 		System.out.println("aaaaaa");
->>>>>>> branch 'master' of https://github.com/gggg907877781/EclipseGit.git
+
 	}
 }
